cfe90c2bd5512323cb6d3cb649c67e72e4e248a0
yujin <c0a23152cc@edu.teu.ac.jp>
2024-06-18 14:54:19 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -33,6 +33,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bm_rct):
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
